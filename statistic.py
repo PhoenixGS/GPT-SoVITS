3c6e96a763f72d24ebb1f50e58c3b912d6330757
PhoenixGS <thestarrydream@gmail.com>
2024-08-09 10:31:09 +0800

    update statistic.py

--- statistic.py
+++ statistic.py
@@ -2,13 +2,17 @@ import librosa
 import os
 from tqdm import tqdm
 
-total_duration = 0.0
 
-lis = os.listdir("data/陆光")
-for li in tqdm(lis):
-    if li.endswith(".wav"):
-        y, sr = librosa.load("data/陆光/"+li)
-        duration = librosa.get_duration(y=y, sr=sr)
-        total_duration += duration
+for name in ["陆光", "程小时", "乔玲"]:
+    total_duration = 0.0
+    path = "data/" + name + "/"
 
-print(total_duration)
+    lis = os.listdir(path)
+    for li in tqdm(lis):
+        if li.endswith(".wav"):
+            y, sr = librosa.load(path + li)
+            duration = librosa.get_duration(y=y, sr=sr)
+            total_duration += duration
+
+    print(f"{name} total duration: {total_duration}")
+    # print(total_duration)
